39609c010e551b5135b557518bdabda4dfec0398
unknown <rishav8541037942@gmail.com>
2022-12-03 13:08:45 +0530

    version 3 to react and comment on linkdin posts

--- content-script.js
+++ content-script.js
@@ -1,25 +1,64 @@
-const scan =()=>{
-    if(window.location.href.includes("/overlay/contact-info")===true){
-        var contact=document.getElementsByClassName("pv-contact-info__ci-container")
-        var arr = []
-        for (let index = 0; index < contact.length; index++) {
-            arr.push(contact[index].innerText)   
-        }
-        chrome.runtime.sendMessage({contact : arr},(response)=>{
-            console.log(response)
-        });
+var evt = new MouseEvent('mouseenter', {
+    'view': window,
+    'bubbles': true,
+    'cancelable': true
+});
+var evt2 = new MouseEvent('mouseleave', {
+    'view': window,
+    'bubbles': true,
+    'cancelable': true
+});
+var temp=0;
+const likeLoop = (like)=>{
+    if(like>0){         
+        setTimeout(()=>{
+            temp=temp+1;
+            let x = Math.floor(Math.random() *7);           
+            document.querySelectorAll(".feed-shared-social-action-bar__action-button .react-button__text ")[temp-1].dispatchEvent(evt);
+            setTimeout(()=>{
+                document.querySelectorAll(".reactions-menu__reaction")[x].click();
+            },500);
+            setTimeout(()=>{
+                document.querySelectorAll(".feed-shared-social-action-bar__action-button .react-button__text ")[temp-1].dispatchEvent(evt2);
+            },1000);
+            likeLoop(like-1);                               
+        }, 3000);
+        
     }else{
-        var pName = document.querySelector("h1.text-heading-xlarge").innerText
-        var pAddress = document.querySelector("span.text-body-small.inline").innerText
-
-        chrome.runtime.sendMessage({name : pName , address : pAddress},(response)=>{
-            console.log(response)
-        });
-
+        return
     }
 }
 
-scan()
+var temp2=0;
+const commentLoop=(comment)=>{
+    if(comment>0){
+        setTimeout(()=>{
+            temp2=temp2+1;
+            document.querySelectorAll(".comment-button")[temp2-1].click();
+            setTimeout(()=>{
+                document.querySelectorAll(".editor-content p")[temp2-1].innerText="CFBR";
+            },500);
+            setTimeout(()=>{
+                document.querySelector(".comments-comment-box__submit-button").click();
+            },1500);
+            commentLoop(comment-1);
+        },3000)
+    }else{
+        return
+    }
+}
+const scan = () =>{
+    chrome.runtime.onMessage.addListener((msgObj,sender,sendResponse)=> {
+        sendResponse({msg:"Recived"});
+        const time =3000*(msgObj.like+1);
+        var max =Math.max(msgObj.like,msgObj.comment)
+        console.log(msgObj);
+        
+        likeLoop(msgObj.like);
+        setTimeout(()=>{
+            commentLoop(msgObj.comment)
+        },time);   
+    });
+}
 
-
-//[“https://www.linkedin.com/in/isanur-sardar-a14106185”,"https://www.linkedin.com/in/bipul-choudhary-8833b0b9",“https://www.linkedin.com/in/rishav-kumar-061ba222b”,"https://www.linkedin.com/in/anjali-k-379948143"]
+scan();
--- index.js
+++ index.js
@@ -5,40 +5,32 @@ const isGivenWebsite= async (ele)=>{
     return tab.url.includes(ele);
 }
 
-const myLoop = (i,arr)=>{         
-    setTimeout(function() {
-        chrome.tabs.update(
-            {url: arr[i]}
-        );
-        i++;                    
-        if (i < arr.length) {          
-            myLoop(i,arr);              
-        }                       
-    }, 5000);
+const mes = (data)=>{
+    let queryOptions = { active: true, lastFocusedWindow: true };
+    chrome.tabs.query(queryOptions,(tab)=>{
+        chrome.tabs.sendMessage(tab[0].id, data, (response)=>{
+            console.log(response);
+        });    
+    });
 }
 
+
 window.addEventListener("load",async ()=>{
-    var data = await isGivenWebsite('linkedin.com')
+    var data = await isGivenWebsite('https://www.linkedin.com/feed/')
+
     if(data=== true){
         document.querySelector('.discription').classList.add('hidden');
         document.querySelector('.title').classList.remove('hidden');
     };
 
-    chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-        sendResponse({responce :"Data Recived"});
-        console.log(JSON.stringify(message))
-    });
     document.getElementById("click_it").addEventListener("click",()=>{
-        var inputData = document.getElementById("input_arr").value
-        var arr= inputData.substring(1,inputData.length-1).split(",");
-        var linkArr=[];
-
-        arr.forEach(item => {
-            let temp =item.trim()
-            linkArr.push(temp.substring(1,temp.length-1))
-            linkArr.push(temp.substring(1,temp.length-1)+"/overlay/contact-info")
-        });
-
-        myLoop(0,linkArr)
+        var like = parseInt(document.getElementById("like").value)
+        var comment = parseInt(document.getElementById("comment").value)
+        var data = {
+            like: like,
+            comment: comment 
+        }
+        
+        mes(data);
     })    
 })
